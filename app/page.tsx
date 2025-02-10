41324373356f12be62f6002386a29adf362e28f1
David Lee <Dai180293@gmail.com>
2025-02-10 12:34:23 +0000

    added: business card to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
     return (
-        <section>
-            <h1>Home page</h1>
+        <section className="h-screen bg-black text-white bg-homeBackground bg-cover grid place-content-center">
+            <article className="space-y-4 p-12 rounded-xl bg-black/65 w-fit">
+                <h1 className="text-xl sm:text-2xl">Computer Repair Shop</h1>
+                <address>
+                    555 Gateway Lane <br />
+                    South Wales, UK
+                </address>
+                <p>Open: 9am - 5pm</p>
+                <p>
+                    <Link href="tel:01443 000000">01443 000000</Link>
+                </p>
+            </article>
         </section>
     );
 }
