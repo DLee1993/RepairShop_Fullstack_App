c5af3a0914de4077d15fb4eb61b06f9175f993d3
David Lee <Dai180293@gmail.com>
2025-02-10 14:03:29 +0000

    added: a div to manage the width of the entire layout

--- app/(repairShopRoutes)/layout.tsx
+++ app/(repairShopRoutes)/layout.tsx
@@ -2,9 +2,9 @@ import Header from "@/components/Header";
 
 export default function RSLayout({ children }: { children: React.ReactNode }) {
     return (
-        <>
+        <div className="max-w-4xl mx-auto">
             <Header />
             <main className="px-4 py-2">{children}</main>
-        </>
+        </div>
     );
 }
