cb3c7ade1034fe3fae3affde63bf98a45c52b049
David Lee <Dai180293@gmail.com>
2025-02-10 14:03:05 +0000

    added nav button components with routing to all pages

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,19 @@
+import { HomeIcon, FileIcon, UsersRoundIcon } from "lucide-react";
+import NavButton from "@/components/NavButton";
+
 export default function Header() {
-  return (
-    <div>Header</div>
-  )
+    return (
+        <header className="animate-slide bg-background h-12 p-2 border-b sticky top-0 flex justify-between">
+            <NavButton href="/home" label="Home" icon={HomeIcon} />
+
+            <ul className="flex gap-2">
+                <li>
+                    <NavButton href="/tickets" label="Tickets" icon={FileIcon} />
+                </li>
+                <li>
+                    <NavButton href="/customers" label="Customers" icon={UsersRoundIcon} />
+                </li>
+            </ul>
+        </header>
+    );
 }
