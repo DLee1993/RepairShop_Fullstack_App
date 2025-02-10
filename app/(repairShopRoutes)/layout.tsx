2e6c5a609dc8b84daec4b4e279073ac7314074ee
David Lee <Dai180293@gmail.com>
2025-02-10 13:29:11 +0000

    created: header component, added: header component to layout

--- app/(repairShopRoutes)/layout.tsx
+++ app/(repairShopRoutes)/layout.tsx
@@ -1,7 +1,9 @@
+import Header from "@/components/Header";
+
 export default function RSLayout({ children }: { children: React.ReactNode }) {
     return (
         <>
-            {/* Header here */}
+            <Header />
             <main className="px-4 py-2">{children}</main>
         </>
     );
