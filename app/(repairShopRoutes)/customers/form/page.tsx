a134ace4645defb1efa157b0ca48eef0feefa73e
David Lee <Dai180293@gmail.com>
2025-03-11 18:44:24 +0000

    added: customer form page

---
+++ app/(repairShopRoutes)/customers/form/page.tsx
@@ -0,0 +1,36 @@
+import BackButton from "@/components/BackButton";
+import { getCustomer } from "@/lib/queries/getCustomer";
+
+export default async function CustomerFormPage({
+    searchParams,
+}: {
+    searchParams: Promise<{ [key: string]: string | undefined }>;
+}) {
+    try {
+        const { customerId } = await searchParams;
+
+        // Edit a customer form
+        if (customerId) {
+            const customer = await getCustomer(parseInt(customerId));
+
+            if (!customer) {
+                return (
+                    <>
+                        <h2 className="text-2xl mb-2">Customer id: #{customerId} not found</h2>
+                        <BackButton title="Go back" variant="default" />
+                    </>
+                );
+            }
+
+            console.log(customer)
+
+            // exisiting customer form component goes here //
+        } else {
+            // new customer form component goes here //
+        }
+    } catch (error) {
+        if (error instanceof Error) {
+            throw error;
+        }
+    }
+}
