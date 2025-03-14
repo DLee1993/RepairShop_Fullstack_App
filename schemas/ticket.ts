184b38c76d306199bf1ffafb6220019eecd6a785
David Lee <Dai180293@gmail.com>
2025-03-14 17:56:15 +0000

    added form schemas

---
+++ schemas/ticket.ts
@@ -0,0 +1,23 @@
+import { createInsertSchema, createSelectSchema } from "drizzle-zod";
+import { tickets } from "@/db/schema";
+import { z } from "zod";
+
+// Defines the shape of data to be inserted into the database
+// This includes an object with additional validation (zod)
+
+export const insertTicketSchema = createInsertSchema(tickets, {
+    id: z.union([z.number(), z.literal("(New)")]),
+    title: (schema) => schema.min(1, "Title is required"),
+    description: (schema) => schema.min(1, "Description is required"),
+    tech: (schema) => schema.email("Invalid email address"),
+});
+
+// Defines the shape of the data queried from the database
+
+export const selectTicketSchema = createSelectSchema(tickets);
+
+// Get the types of both schemas
+
+export const insertTicketSchemaType = typeof insertTicketSchema._type;
+
+export const selectTicketSchemaType = typeof selectTicketSchema._type;
