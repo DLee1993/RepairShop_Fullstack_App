0a7e0caedb4dae9e4d758a34b0338fd37b9880d0
David Lee <Dai180293@gmail.com>
2025-03-22 08:16:53 +0000

    added custom input form field

---
+++ components/form/CustomFormInput.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { InputHTMLAttributes } from "react";
+import { useFormContext } from "react-hook-form";
+import { FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+// Input props
+// <S> generic type for the schema
+
+interface CustomFormInputProps<S> extends InputHTMLAttributes<HTMLInputElement> {
+    fieldTitle: string;
+    nameInSchema: keyof S & string;
+    className?: string;
+}
+
+export default function CustomFormInput<S>({
+    fieldTitle,
+    nameInSchema,
+    className,
+    ...props
+}: CustomFormInputProps<S>) {
+    const form = useFormContext();
+
+    // render the input field with the form control
+
+    return (
+        <FormField
+            control={form.control}
+            name={nameInSchema}
+            render={({ field }) => (
+                <FormItem className={className}>
+                    <FormLabel className="text-base" htmlFor={nameInSchema}>
+                        {fieldTitle}
+                    </FormLabel>
+                    <FormControl>
+                        <Input
+                            id={nameInSchema}
+                            className={`w-full max-w-xs disabled:text-blue-500 dark:disabled:text-green-500 disabled:opacity-75 ${className}`}
+                            {...props}
+                            {...field} // pass the field props to the input - onChange, onBlur, ref, etc.
+                        ></Input>
+                    </FormControl>
+                    <FormMessage className="text-red-500" /> {/* error message */}
+                </FormItem>
+            )}
+        />
+    );
+}
